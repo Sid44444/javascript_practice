de875b51145e563a3e0f2af97c83b8afd1b0160d
Sarah Adlington <cheshire6655@icloud.com>
2019-03-25 21:48:07 +0000

    15 left

--- challenges/week1.js
+++ challenges/week1.js
@@ -12,8 +12,11 @@ function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
   // Add your code here!
-  return firstName(0).lastName(0);
+  
+//return firstName[0]+lastName[0].substring(0, 1).toUpperCase();
+      
 }
+
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
@@ -33,7 +36,22 @@ function getSalePrice(originalPrice, reduction) {
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
   // Add your code here!
-}
+
+    var middleC = "";
+    
+    if (str.length%2 === 0) {
+      middleC +=  str[(str.length/2)-1];
+      middleC += str[str.length/2];
+    }
+    
+    else if (str.length%2 === 1) {
+      middleC += str[Math.floor(str.length/2)]
+    }
+    return middleC;
+      
+    }
+    
+
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
